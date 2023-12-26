5fbfbc6d43db09928123bcb5b04eb105de207496
BYEONGRYEOL <lyulbyung@gmail.com>
2023-12-26 21:06:52 +0900

    jwtTokenSubjectDto null check 추가

--- src/main/java/com/hanghae/lemonairchat/config/WebSocketConfig.java
+++ src/main/java/com/hanghae/lemonairchat/config/WebSocketConfig.java
@@ -18,48 +18,52 @@ import reactor.core.publisher.Mono;
 @Configuration
 public class WebSocketConfig {
 
+	@Bean
+	public WebSocketHandlerAdapter webSocketHandlerAdapter(JwtUtil jwtUtil) {
+		return new WebSocketHandlerAdapter(webSocketService(jwtUtil));
+	}
 
-    @Bean
-    public WebSocketHandlerAdapter webSocketHandlerAdapter(JwtUtil jwtUtil) {
-        return new WebSocketHandlerAdapter(webSocketService(jwtUtil));
-    }
+	@Bean
+	public WebSocketService webSocketService(JwtUtil jwtUtil) {
+		HandshakeWebSocketService webSocketService = new HandshakeWebSocketService() {
+			@Override
+			public Mono<Void> handleRequest(ServerWebExchange exchange, WebSocketHandler handler) {
+				String jwtAccessToken = exchange.getRequest().getHeaders().getFirst("Authorization");
 
-    @Bean
-    public WebSocketService webSocketService(JwtUtil jwtUtil) {
-        HandshakeWebSocketService webSocketService = new HandshakeWebSocketService() {
-            @Override
-            public Mono<Void> handleRequest(ServerWebExchange exchange, WebSocketHandler handler) {
-                String jwtAccessToken = exchange.getRequest()
-                    .getHeaders()
-                    .getFirst("Authorization");
-
-                String nicknameAttr;
-                String loginIdAttr;
-                Role role;
-                if(!ObjectUtils.isEmpty(jwtAccessToken)){
-                    JwtTokenSubjectDto jwtTokenSubjectDto = jwtUtil.getSubjectFromToken(jwtAccessToken);
-                    loginIdAttr = jwtTokenSubjectDto.getLoginId();
-                    nicknameAttr = jwtTokenSubjectDto.getNickname();
-                    role = Role.MEMBER;
-                    // TODO: 2023-12-26 방송의 방장 or Manager 인지 파악하는 로직 추가
-                } else {
-					role = Role.NOT_LOGIN;
-					nicknameAttr = "";
-					loginIdAttr = "";
+				String nicknameAttr ;
+				final String loginIdAttr;
+				Role role;
+				if (!ObjectUtils.isEmpty(jwtAccessToken)) {
+					JwtTokenSubjectDto jwtTokenSubjectDto = jwtUtil.getSubjectFromToken(jwtAccessToken);
+					if (jwtTokenSubjectDto != null) {
+						loginIdAttr = jwtTokenSubjectDto.getLoginId();
+						nicknameAttr = jwtTokenSubjectDto.getNickname();
+						role = Role.MEMBER;
+						return exchange.getSession().flatMap(session -> {
+							session.getAttributes().put("Role", role.toString());
+							session.getAttributes().put("LoginId", loginIdAttr);
+							session.getAttributes().put("Nickname", nicknameAttr);
+							return super.handleRequest(exchange, handler);
+						});
+						// TODO: 2023-12-26 방송의 방장 or Manager 인지 파악하는 로직 추가
+					}
 				}
 
-				return exchange.getSession()
-                    .flatMap(session -> {
-                        session.getAttributes().put("Role", role.toString());
-                        session.getAttributes().put("LoginId", loginIdAttr);
-                        session.getAttributes().put("Nickname", nicknameAttr);
-                        return super.handleRequest(exchange, handler);
-                    });
+				role = Role.NOT_LOGIN;
+				nicknameAttr = "";
+				loginIdAttr = "";
 
-            }
-        };
+				return exchange.getSession().flatMap(session -> {
+					session.getAttributes().put("Role", role.toString());
+					session.getAttributes().put("LoginId", loginIdAttr);
+					session.getAttributes().put("Nickname", nicknameAttr);
+					return super.handleRequest(exchange, handler);
+				});
 
-        webSocketService.setSessionAttributePredicate(s -> true);
-        return webSocketService;
-    }
+			}
+		};
+
+		webSocketService.setSessionAttributePredicate(s -> true);
+		return webSocketService;
+	}
 }
