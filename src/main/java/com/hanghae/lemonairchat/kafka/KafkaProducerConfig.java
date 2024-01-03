b1efa9d3ea6e1b267e47baac62573a56fdf15adc
BYEONGRYEOL <lyulbyung@gmail.com>
2024-01-03 22:23:43 +0900

    배포준비 kafka 관련 property 정리중

--- src/main/java/com/hanghae/lemonairchat/kafka/KafkaProducerConfig.java
+++ src/main/java/com/hanghae/lemonairchat/kafka/KafkaProducerConfig.java
@@ -2,7 +2,10 @@ package com.hanghae.lemonairchat.kafka;
 
 import com.hanghae.lemonairchat.entity.Chat;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
@@ -12,11 +15,16 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate;
+
+import jakarta.annotation.PostConstruct;
 import reactor.kafka.sender.SenderOptions;
 
 @Configuration
 public class KafkaProducerConfig {
-    private String bootstrapServer = "localhost:9091,localhost:9092,localhost:9093";
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private List<String> bootstrapServers;
+    // private String bootstrapServer = "localhost:9091,localhost:9092,localhost:9093";
 
     @Value("${spring.kafka.producer.key-serializer}")
     private String keySerializer;
@@ -24,11 +32,17 @@ public class KafkaProducerConfig {
     @Value("${spring.kafka.producer.value-serializer}")
     private String valueSerializer;
 
+    @PostConstruct
+    public void init(){
+
+        System.out.println(String.join(",", bootstrapServers));
+    }
+
     @Bean
     public ReactiveKafkaProducerTemplate<String, Chat> reactiveKafkaProducerTemplate(
         KafkaProperties properties) {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, String.join(",", bootstrapServers));
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, keySerializer);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
 
--- src/main/java/com/hanghae/lemonairchat/kafka/KafkaTopicManager.java
+++ src/main/java/com/hanghae/lemonairchat/kafka/KafkaTopicManager.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
@@ -17,9 +18,11 @@ public class KafkaTopicManager {
 
     private final AdminClient adminClient;
 
+    @Value("${spring.kafka.admin-client}")
+    private String adminClientHost;
     public KafkaTopicManager() {
         Properties properties = new Properties();
-        properties.setProperty(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9091");
+        properties.setProperty(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, adminClientHost);
 
         this.adminClient = AdminClient.create(properties);
     }
