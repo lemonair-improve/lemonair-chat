0cbba09e8e3acf809224ea12e82cbe42552e3976
munee <121735319+alaneelee@users.noreply.github.com>
2024-01-06 17:36:02 +0900

    feat: Heartbeat 요청 추가

--- src/test/java/com/hanghae/lemonairchat/LemonairChatApplicationTests.java
+++ src/test/java/com/hanghae/lemonairchat/LemonairChatApplicationTests.java
@@ -3,11 +3,11 @@ package com.hanghae.lemonairchat;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class LemonairChatApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class LemonairChatApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
