968624a59f8e81c864a8c9f36503052415b80dae
munee <121735319+alaneelee@users.noreply.github.com>
2024-01-08 11:56:20 +0900

    비동기가 되어라

--- src/main/java/com/hanghae/lemonairchat/handler/ChatWebSocketHandler.java
+++ src/main/java/com/hanghae/lemonairchat/handler/ChatWebSocketHandler.java
@@ -23,59 +23,70 @@ import reactor.core.scheduler.Schedulers;
 @Component
 @Slf4j
 public class ChatWebSocketHandler implements WebSocketHandler {
-	private final ChatRepository chatRepository;
-	private final KafkaTopicManager kafkaTopicManager;
-	private final KafkaConsumerService kafkaConsumerService;
-	private final ReactiveKafkaProducerTemplate<String, Chat> reactiveKafkaProducerTemplate;
 
-	@Override
-	public Mono<Void> handle(WebSocketSession session) {
-		final String role = (String)session.getAttributes().get("Role");
-		final String id = (String)session.getAttributes().get("id");
-		final String nickname = (String)session.getAttributes().getOrDefault("Nickname", "익명의 사용자");
-		final String roomId;
+    private final ChatRepository chatRepository;
+    private final KafkaTopicManager kafkaTopicManager;
+    private final KafkaConsumerService kafkaConsumerService;
+    private final ReactiveKafkaProducerTemplate<String, Chat> reactiveKafkaProducerTemplate;
 
-		log.info("{}의 websockethandler 입장", nickname);
-		String getUrl = session.getHandshakeInfo().getUri().getPath();
-		String[] pathSegments = getUrl.split("/");
+    @Override
+    public Mono<Void> handle(WebSocketSession session) {
+        final String role = (String) session.getAttributes().get("Role");
+        final String id = (String) session.getAttributes().get("id");
+        final String nickname = (String) session.getAttributes()
+            .getOrDefault("Nickname", "익명의 사용자");
+        final String roomId;
 
-		if (pathSegments.length > 2) {
-			roomId = pathSegments[2];
-		} else {
-			return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 경로입니다"));
-		}
+        log.info("{}의 websockethandler 입장", nickname);
+        String getUrl = session.getHandshakeInfo().getUri().getPath();
+        String[] pathSegments = getUrl.split("/");
 
-		kafkaTopicManager.createTopic(roomId, 3, (short)1).publishOn(Schedulers.boundedElastic()).subscribe();
+        if (pathSegments.length > 2) {
+            roomId = pathSegments[2];
+        } else {
+            return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "잘못된 경로입니다"));
+        }
 
-		ReactiveKafkaConsumerTemplate<String, Chat> consumer = kafkaConsumerService.reactiveKafkaConsumerTemplate(
-			roomId);
+        kafkaTopicManager.createTopic(roomId, 3, (short) 1).publishOn(Schedulers.boundedElastic())
+            .subscribe();
 
-		// log.info("consumer: {}", consumer);
+        ReactiveKafkaConsumerTemplate<String, Chat> consumer = kafkaConsumerService.reactiveKafkaConsumerTemplate(
+            roomId);
 
 		consumer.receiveAutoAck()
-			// .publishOn(Schedulers.boundedElastic())
+			.subscribeOn(Schedulers.boundedElastic())
 			.map(ConsumerRecord::value)
 			.filter(chat -> !chat.getMessage().equals("heartbeat"))
 			.flatMap(chat -> {
 				log.info("{} 메세지를 받아서 웹소켓에 보내줄거야 클라이언트가 보도록", chat.getMessage());
-				return session.send(Mono.just(session.textMessage(chat.getSender() + ":" + chat.getMessage())))
+				return session.send(
+						Mono.just(session.textMessage(chat.getSender() + ":" + chat.getMessage())))
 					.log()
-					.doOnError(throwable -> log.error(" 메세지 전송중 에러 발생 : {}", throwable.getMessage()));
+					.doOnError(
+						throwable -> log.error(" 메세지 전송중 에러 발생 : {}", throwable.getMessage()));
 			})
-			.doOnError(throwable -> log.error("something bad happened while consuming : {}", throwable.getMessage()))
+			.doOnError(throwable -> log.error("something bad happened while consuming : {}",
+				throwable.getMessage()))
 			.subscribe();
 
-		return session.receive()
-			// .publishOn(Schedulers.boundedElastic())
-			.doFinally(signalType -> {
-			log.info("클라이언트의 세션 종료 요청 : " + signalType.toString());
-			session.close().log().subscribe();
-		}).flatMap(webSocketMessage -> {
-			String message = webSocketMessage.getPayloadAsText();
-			Chat chat = new Chat(message, nickname, roomId);
-			log.info("소켓의 채팅 {} 전송을 받아서 db에 저장하고 카프카에 뿌릴거야", chat.getMessage());
-			return chatRepository.save(chat)
-				.flatMap(savedChat -> reactiveKafkaProducerTemplate.send(roomId, savedChat).then());
-		}).then();
-	}
+        // log.info("consumer: {}", consumer);
+
+
+
+        return session.receive()
+            .doFinally(signalType -> {
+                log.info("클라이언트의 세션 종료 요청 : " + signalType.toString());
+                session.close().log().subscribe();
+    			consumer.paused().subscribe();
+            }).flatMap(webSocketMessage -> {
+                String message = webSocketMessage.getPayloadAsText();
+                Chat chat = new Chat(message, nickname, roomId);
+                log.info("소켓의 채팅 {} 전송을 받아서 db에 저장하고 카프카에 뿌릴거야", chat.getMessage());
+                chatRepository.save(chat).publishOn(Schedulers.boundedElastic())
+                    .flatMap(savedChat ->
+                        reactiveKafkaProducerTemplate.send(roomId, savedChat)
+                        .then()).subscribe();
+                return Mono.empty();
+            }).then();
+    }
 }
