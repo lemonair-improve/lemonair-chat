79be886a512c9a563dcbafd9733d2e67101a356c
BYEONGRYEOL <lyulbyung@gmail.com>
2024-01-09 16:25:34 +0900

    websession 유지하지않도록 WebScoketConfig에서 delegate 패턴을 이용해서 WebSocketSession으로 연결 유지, session이 닫혀도 consumer에 null 할당하지 않는 ㅅ버전

--- src/main/java/com/hanghae/lemonairchat/config/WebSocketConfig.java
+++ src/main/java/com/hanghae/lemonairchat/config/WebSocketConfig.java
@@ -9,7 +9,6 @@ import org.springframework.web.reactive.socket.server.WebSocketService;
 import org.springframework.web.reactive.socket.server.support.HandshakeWebSocketService;
 import org.springframework.web.reactive.socket.server.support.WebSocketHandlerAdapter;
 import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebSession;
 
 import com.hanghae.lemonairchat.constants.Role;
 import com.hanghae.lemonairchat.util.JwtTokenSubjectDto;
@@ -30,13 +29,6 @@ public class WebSocketConfig {
 	@Bean
 	public WebSocketService webSocketService(JwtUtil jwtUtil) {
 		HandshakeWebSocketService webSocketService = new HandshakeWebSocketService() {
-			int index = 0;
-
-			private static void setAttributes(WebSession session, JwtTokenSubjectDto dto) {
-				session.getAttributes().put("Role", Role.MEMBER.toString());
-				session.getAttributes().put("LoginId", dto.getLoginId());
-				session.getAttributes().put("Nickname", dto.getNickname());
-			}
 
 			private static void setAttributes(WebSocketSession session, JwtTokenSubjectDto dto) {
 				session.getAttributes().put("Role", Role.MEMBER.toString());
@@ -47,7 +39,6 @@ public class WebSocketConfig {
 			@Override
 			public Mono<Void> handleRequest(ServerWebExchange exchange, WebSocketHandler handler) {
 				log.info("연결 요청");
-				// log.info("exchange.getRequest().getURI().getPath() : " + exchange.getRequest().getURI().getPath());
 				WebSocketHandler decorator = session -> {
 					String path = exchange.getRequest().getURI().getPath();
 					String jwtChatAccessToken = path.substring(path.lastIndexOf("/") + 1);
@@ -75,63 +66,6 @@ public class WebSocketConfig {
 				};
 
 				return super.handleRequest(exchange, decorator);
-
-				// return exchange.getSession()
-				// 	.flatMap(session -> {
-				// 		String path = exchange.getRequest().getURI().getPath();
-				// 		String jwtChatAccessToken = path.substring(path.lastIndexOf("/") + 1);
-				// 		if (ObjectUtils.isEmpty(jwtChatAccessToken)) {
-				// 			return Mono.error(new RuntimeException("chatAccessToken path param이 공백 문자열입니다."));
-				// 		}
-				// 		if (jwtChatAccessToken.startsWith("VU")) {
-				// 			setAttributes(session, JwtTokenSubjectDto.builder()
-				// 				.loginId(jwtChatAccessToken)
-				// 				.nickname(jwtChatAccessToken)
-				// 				.build());
-				// 			return super.handleRequest(exchange, handler);
-				// 		} else if (!"notlogin".equals(jwtChatAccessToken)) {
-				// 			log.info("로그인한 사용자의 채팅 웹 소켓 연결 요청");
-				//
-				// 			setAttributes(session, jwtUtil.getSubjectFromToken(jwtChatAccessToken));
-				// 			return super.handleRequest(exchange, handler);
-				// 			// TODO: 2023-12-26 방송의 방장 or Manager 인지 파악하는 로직 추가
-				// 		} else {
-				// 			log.info("로그인하지 않은 사용자의 채팅 웹 소켓 연결 요청");
-				// 			session.getAttributes().put("Role", Role.NOT_LOGIN.toString());
-				// 			return super.handleRequest(exchange, handler);
-				// 		}
-				// 	});
-
-				// String path = exchange.getRequest().getURI().getPath();
-				// String jwtChatAccessToken = path.substring(path.lastIndexOf("/") + 1);
-				// // log.info("jwtChatAccessToken : " + jwtChatAccessToken);
-				//
-				// if (ObjectUtils.isEmpty(jwtChatAccessToken)) {
-				// 	throw new RuntimeException("chatAccessToken path param이 공백 문자열입니다.");
-				// }
-				// if (jwtChatAccessToken.startsWith("VU")) {
-				// 	return exchange.getSession().flatMap(session -> {
-				// 		setAttributes(session, JwtTokenSubjectDto.builder()
-				// 			.loginId(jwtChatAccessToken)
-				// 			.nickname(jwtChatAccessToken)
-				// 			.build());
-				// 		return super.handleRequest(exchange, handler);
-				// 	});
-				// }
-				// if (!"notlogin".equals(jwtChatAccessToken)) {
-				// 	log.info("로그인한 사용자의 채팅 웹 소켓 연결 요청");
-				// 	return exchange.getSession().flatMap(session -> {
-				// 		setAttributes(session, jwtUtil.getSubjectFromToken(jwtChatAccessToken));
-				// 		return super.handleRequest(exchange, handler);
-				// 	});
-				// 	// TODO: 2023-12-26 방송의 방장 or Manager 인지 파악하는 로직 추가
-				// } else {
-				// 	log.info("로그인하지 않은 사용자의 채팅 웹 소켓 연결 요청");
-				// 	return exchange.getSession().flatMap(session -> {
-				// 		session.getAttributes().put("Role", Role.NOT_LOGIN.toString());
-				// 		return super.handleRequest(exchange, handler);
-				// 	});
-				// }
 			}
 		};
 
