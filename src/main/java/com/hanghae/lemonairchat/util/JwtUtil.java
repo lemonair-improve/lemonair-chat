2b977acbb621f2594522d205899fdab3167e0b53
BYEONGRYEOL <lyulbyung@gmail.com>
2024-01-10 18:30:34 +0900

    aws 마비로 임시 수정되었던 부분 롤백, JwtUtil 사용하지 않는 메서드 제거

--- src/main/java/com/hanghae/lemonairchat/util/JwtUtil.java
+++ src/main/java/com/hanghae/lemonairchat/util/JwtUtil.java
@@ -24,39 +24,15 @@ import reactor.core.publisher.Mono;
 public class JwtUtil {
 
 	public static final String BEARER_PREFIX = "Bearer ";
-	// @Value("${jwt.secretKey}")
-	// private String secretKey;
-	private String secretKey = "6rCcIOyWtOydtOyXhuuEpOqxjSDsvJzsp4DrqbTrkJjsp4Ag7J206rKMIOynp+uLpOqzoCDruYgg7IOd7ISx7J20IOyViOuPvD8=";
+
+	@Value("${jwt.secretKey}")
+	private String secretKey;
 	private Key key;
 
 	@PostConstruct
 	public void init() {
 		byte[] bytes = Base64.getDecoder().decode(secretKey);
 		key = Keys.hmacShaKeyFor(bytes);
-	}
-
-	public String substringToken(String tokenValue) {
-		if (StringUtils.hasText(tokenValue) && tokenValue.startsWith(BEARER_PREFIX)) {
-			return tokenValue.substring(7);
-		}
-		throw new NullPointerException("Not Found Token");
-	}
-
-	public Mono<Boolean> validateToken(String token) {
-		try {
-			Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
-			return Mono.just(true);
-		} catch (SecurityException | MalformedJwtException | SignatureException e) {
-			log.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.");
-		} catch (ExpiredJwtException e) {
-			log.error("Expired JWT token, 만료된 JWT token 입니다.");
-			return Mono.just(false);
-		} catch (UnsupportedJwtException e) {
-			log.error("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.");
-		} catch (IllegalArgumentException e) {
-			log.error("JWT claims is empty, 잘못된 JWT 토큰 입니다.");
-		}
-		return Mono.just(false);
 	}
 
 	public JwtTokenSubjectDto getSubjectFromToken(String token) {
